40913eb7e301e97076367fe92aff555027771920
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-01 14:30:35 -0500

    ci: 👷 add eslint

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+]);
