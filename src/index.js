ea6ad74ad029458aee81f9db4e8ee46d120cf11d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-01 22:28:43 -0500

    feat: ✨ add GoogleCalendarEventFetcher class

--- src/index.js
+++ src/index.js
@@ -4,17 +4,40 @@
 export class GoogleCalendarEventFetcher {
   #apiKey;
   #calendarId;
+  #fetch;
 
   /** @param {Options} options */
-  constructor({ apiKey, calendarId }) {
+  constructor({ apiKey, calendarId, fetch }) {
     if (typeof apiKey !== "string" || apiKey == "") {
       throw new Error("apiKey is a required string");
     }
     if (typeof calendarId !== "string" || calendarId == "") {
       throw new Error("calendarId is a required string");
     }
+    if (fetch != undefined && typeof fetch !== "function") {
+      throw new Error("fetch must be a function");
+    }
     this.#apiKey = apiKey;
     this.#calendarId = calendarId;
+    this.#fetch = fetch ?? globalThis.fetch;
+  }
+
+  /**
+   * Fetches all the events within a given range.
+   * @param {Date} from The start of the time range to fetch events for.
+   * @param {Date} to The end of the time range to fetch events for.
+   * @returns {Promise<object[]>} A promise that resolves to an array of events.
+   */
+  async fetchEvents(from, to) {
+    const url = new URL(`https://www.googleapis.com/calendar/v3/calendars/${this.#calendarId}/events`);
+    url.searchParams.append("key", this.#apiKey);
+    url.searchParams.append("timeMin", from.toISOString());
+    url.searchParams.append("timeMax", to.toISOString());
+    const response = await this.#fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch events: ${response.status} ${response.statusText}`, { cause: response });
+    }
+    return await response.json();
   }
 }
 
@@ -24,4 +47,5 @@ export default GoogleCalendarEventFetcher;
  * @typedef {Object} Options
  * @property {string} apiKey The API key for accessing Google Calendar API.
  * @property {string} calendarId The ID of the Google Calendar to fetch events from.
+ * @property {(url: URL) => Promise<Response>} [fetch=fetch] Optional fetch function to use for making HTTP requests.
  */
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,17 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import defaultExport, { GoogleCalendarEventFetcher } from "./index.js";
 
 const API_KEY = "example_api_key";
 const CALENDAR_ID = "example_calendar@group.calendar.google.com";
+
+/**
+ * Creates a mock for the fetch function.
+ * @param {Response} response
+ * @returns {import("vitest").Mock<Required<import("./index.js").Options>["fetch"]>}
+ */
+function mockFetch(response) {
+  return vi.fn(() => Promise.resolve(response));
+}
 
 describe("GoogleCalendarEventFetcher", () => {
   it("should be exported as default and with a name", () => {
@@ -19,5 +28,55 @@ describe("GoogleCalendarEventFetcher", () => {
     expect(() => new GoogleCalendarEventFetcher({ apiKey: API_KEY })).toThrow();
     // @ts-expect-error
     expect(() => new GoogleCalendarEventFetcher({ calendarId: CALENDAR_ID })).toThrow();
+  });
+
+  it("should accept a fetch function", () => {
+    const fetch = mockFetch(Response.json([]));
+
+    new GoogleCalendarEventFetcher({
+      apiKey: API_KEY,
+      calendarId: CALENDAR_ID,
+      fetch: fetch,
+    });
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+
+  it("should require fetch to be a function if provided", () => {
+    expect(
+      () =>
+        new GoogleCalendarEventFetcher({
+          apiKey: API_KEY,
+          calendarId: CALENDAR_ID,
+          // @ts-expect-error
+          fetch: "not a function",
+        }),
+    ).toThrow();
+  });
+
+  it("should fetch events within a given range", async () => {
+    const mockEvents = {
+      kind: "calendar#events",
+      items: [
+        { kind: "calendar#event", id: "1", summary: "Event 1" },
+        { kind: "calendar#event", id: "2", summary: "Event 2" },
+      ],
+    };
+    const fetch = mockFetch(Response.json(mockEvents));
+    const fetcher = new GoogleCalendarEventFetcher({ apiKey: API_KEY, calendarId: CALENDAR_ID, fetch });
+    const from = new Date("2026-01-01T00:00:00Z");
+    const to = new Date("2026-01-31T23:59:59Z");
+
+    const events = await fetcher.fetchEvents(from, to);
+
+    expect(fetch).toHaveBeenCalledOnce();
+    const calledUrl = fetch.mock.calls[0][0];
+    expect(calledUrl.origin).toBe("https://www.googleapis.com");
+    expect(calledUrl.pathname).toBe(`/calendar/v3/calendars/${CALENDAR_ID}/events`);
+    expect(calledUrl.searchParams.get("key")).toBe(API_KEY);
+    expect(calledUrl.searchParams.get("timeMin")).toBe(from.toISOString());
+    expect(calledUrl.searchParams.get("timeMax")).toBe(to.toISOString());
+
+    expect(events).toEqual(mockEvents);
   });
 });
