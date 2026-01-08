10a0c699a89f9bef31dd5825f3f3c27b8391ad10
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-07 22:01:55 -0500

    feat: ✨ add RangeSet for tracking overlapping ranges

---
+++ src/range-set.js
@@ -0,0 +1,168 @@
+export class RangeSet {
+  /**
+   * All the ranges currently in the set.
+   * Each even index is the start of a range.
+   * Each odd index is the end of a range.
+   * Ranges are arranged in ascending order.
+   * @type {number[]}
+   */
+  #ranges = [];
+
+  /** @param {Iterable<Range>} ranges*/
+  constructor(ranges = []) {
+    for (const range of ranges) {
+      this.addRange(range);
+    }
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.#ranges.length; i += 2) {
+      yield /** @type {Range} */ ([this.#ranges[i], this.#ranges[i + 1]]);
+    }
+  }
+
+  /**
+   * An array with the ranges in the set.
+   * @type {Range[]}
+   */
+  get ranges() {
+    return [...this];
+  }
+
+  /**
+   * Adds a range to the set.
+   * @param {Range} range
+   */
+  addRange(range) {
+    if (!isValidRange(range)) {
+      throw new Error("Invalid Range");
+    }
+
+    const [start, end] = range;
+
+    const firstDeleteIndex = this.#getUpperBoundIndex(start);
+    if (firstDeleteIndex == null) {
+      this.#ranges.push(start, end);
+      return;
+    }
+
+    const lastDeleteIndex = this.#getLowerBoundIndex(end);
+    if (lastDeleteIndex == null) {
+      this.#ranges.unshift(start, end);
+      return;
+    }
+
+    /** @type {number[]} */
+    const newValues = [];
+
+    if (firstDeleteIndex % 2 == 0) {
+      newValues.push(start);
+    }
+
+    if (lastDeleteIndex % 2 != 0) {
+      newValues.push(end);
+    }
+
+    this.#ranges.splice(firstDeleteIndex, lastDeleteIndex - firstDeleteIndex + 1, ...newValues);
+  }
+
+  /**
+   * Checks if a range is completely covered by the set.
+   * @param {Range} range
+   * @returns {boolean}
+   */
+  hasRange(range) {
+    if (!isValidRange(range)) {
+      throw new Error("Invalid Range");
+    }
+
+    const [start, end] = range;
+
+    const lowerBound = this.#getLowerBoundIndex(start);
+
+    if (lowerBound == null || lowerBound % 2 != 0) {
+      return false;
+    }
+
+    return end <= this.#ranges[lowerBound + 1];
+  }
+
+  /**
+   * Gets the index of the largest value in `#ranges` that is less than or equal to a given value
+   * @param {number} target the value to find
+   * @returns {number|null} the index of the largest value that is less than or equal to the target value
+   * or null if the target value smaller than the smallest value
+   */
+  #getLowerBoundIndex(target) {
+    if (this.#ranges.length == 0 || this.#ranges[0] > target) {
+      return null;
+    }
+    if (this.#ranges[this.#ranges.length - 1] <= target) {
+      return this.#ranges.length - 1;
+    }
+
+    /** the index of a value that is less than or equal to the target value */
+    let lowerBound = 0;
+    /** the index of a value that is greater than the target value */
+    let upperBound = this.#ranges.length - 1;
+
+    while (upperBound - lowerBound > 1) {
+      const index = Math.floor((lowerBound + upperBound) / 2);
+      if (this.#ranges[index] > target) {
+        upperBound = index;
+      } else {
+        lowerBound = index;
+      }
+    }
+
+    return lowerBound;
+  }
+
+  /**
+   * Gets the index of the smallest value in `#ranges` that is greater than or equal to a given value
+   * @param {number} target the value to find
+   * @returns {number|null} the index of the smallest value that is greater than or equal to the target value
+   * or null if the target value larger than the largest value
+   */
+  #getUpperBoundIndex(target) {
+    if (this.#ranges.length == 0 || this.#ranges[this.#ranges.length - 1] < target) {
+      return null;
+    }
+    if (this.#ranges[0] >= target) {
+      return 0;
+    }
+
+    /** the index of a value that is less than the target value */
+    let lowerBound = 0;
+    /** the index of a value that is greater than or equal to the target value */
+    let upperBound = this.#ranges.length - 1;
+
+    while (upperBound - lowerBound > 1) {
+      const index = Math.ceil((lowerBound + upperBound) / 2);
+      if (this.#ranges[index] < target) {
+        lowerBound = index;
+      } else {
+        upperBound = index;
+      }
+    }
+
+    return upperBound;
+  }
+}
+
+/**
+ * check if an object is a valid range
+ * @param {unknown} range
+ * @returns {range is Range}
+ */
+export function isValidRange(range) {
+  return (
+    Array.isArray(range) &&
+    range.length === 2 &&
+    typeof range[0] === "number" &&
+    typeof range[1] === "number" &&
+    range[0] <= range[1]
+  );
+}
+
+/** @typedef {[start: number, end: number]} Range */
---
+++ src/range-set.test.js
@@ -0,0 +1,237 @@
+import { describe, expect, it } from "vitest";
+import { isValidRange, RangeSet } from "./range-set.js";
+
+/** @import { Range } from "./range-set.js" */
+
+describe("isValidRange", () => {
+  it("returns true for valid ranges", () => {
+    expect(isValidRange([1, 2])).toBe(true);
+    expect(isValidRange([18, 56789])).toBe(true);
+    expect(isValidRange([1, 1])).toBe(true);
+    expect(isValidRange([0, 0])).toBe(true);
+    expect(isValidRange([-1000, -100])).toBe(true);
+    expect(isValidRange([Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER])).toBe(true);
+    expect(isValidRange([-Number.MIN_VALUE, Number.MIN_VALUE])).toBe(true);
+    expect(isValidRange([-Number.EPSILON, Number.EPSILON])).toBe(true);
+    expect(isValidRange([-Number.MAX_VALUE, Number.MAX_VALUE])).toBe(true);
+    expect(isValidRange([Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY])).toBe(true);
+    expect(isValidRange([-0, 0])).toBe(true);
+  });
+
+  it("returns false for invalid ranges", () => {
+    expect(isValidRange(null)).toBe(false);
+    expect(isValidRange(undefined)).toBe(false);
+    expect(isValidRange("[1, 2]")).toBe(false);
+    expect(isValidRange(["1", "2"])).toBe(false);
+    expect(isValidRange({ [0]: 1, [1]: 2, length: 2 })).toBe(false);
+    expect(isValidRange([new Date(), new Date()])).toBe(false);
+    expect(isValidRange([2, 1])).toBe(false);
+    expect(isValidRange([NaN, NaN])).toBe(false);
+    expect(isValidRange([Number.MIN_VALUE, -Number.MIN_VALUE])).toBe(false);
+    expect(isValidRange([Number.EPSILON, 0])).toBe(false);
+  });
+});
+
+describe("RangeSet", () => {
+  describe("constructor", () => {
+    it("creates an empty set when given no arguments", () => {
+      const set = new RangeSet();
+      expect(set.ranges).toEqual([]);
+    });
+
+    it("can be initialize with an array of ranges", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [4, 5],
+      ]);
+      expect(set.ranges).toEqual([
+        [1, 2],
+        [4, 5],
+      ]);
+    });
+
+    it("can be initialize with a set of ranges", () => {
+      const set = new RangeSet(
+        new Set([
+          [1, 2],
+          [4, 5],
+        ]),
+      );
+      expect(set.ranges).toEqual([
+        [1, 2],
+        [4, 5],
+      ]);
+    });
+
+    it("simplifies ranges during initialization", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [4, 6],
+        [8, 9],
+        [2, 3],
+        [5, 6],
+        [5, 8],
+      ]);
+      expect(set.ranges).toEqual([
+        [1, 3],
+        [4, 9],
+      ]);
+    });
+  });
+
+  describe("[Symbol.iterator]", () => {
+    it("iterates through simplified ranges", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [5, 6],
+        [4, 5],
+      ]);
+
+      const iterator = set[Symbol.iterator]();
+      expect(iterator.next()).toEqual({ done: false, value: [1, 2] });
+      expect(iterator.next()).toEqual({ done: false, value: [4, 6] });
+      expect(iterator.next()).toEqual({ done: true, value: undefined });
+      expect(iterator.next()).toEqual({ done: true, value: undefined });
+    });
+
+    it("has iterator helper functions", () => {
+      const set = new RangeSet();
+
+      const iterator = set[Symbol.iterator]();
+
+      expect(iterator.map).toBeTypeOf("function");
+      expect(iterator.filter).toBeTypeOf("function");
+      expect(iterator.reduce).toBeTypeOf("function");
+      expect(iterator.toArray).toBeTypeOf("function");
+    });
+  });
+
+  describe("addRange", () => {
+    it("adds a range to the set", () => {
+      const set = new RangeSet();
+
+      set.addRange([1, 2]);
+      expect(set.ranges).toEqual([[1, 2]]);
+
+      set.addRange([5, 6]);
+      expect(set.ranges).toEqual([
+        [1, 2],
+        [5, 6],
+      ]);
+
+      set.addRange([3, 4]);
+      expect(set.ranges).toEqual([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]);
+    });
+
+    it("ignores duplicates", () => {
+      const set = new RangeSet();
+
+      set.addRange([1, 2]);
+      expect(set.ranges).toEqual([[1, 2]]);
+
+      set.addRange([1, 2]);
+      expect(set.ranges).toEqual([[1, 2]]);
+    });
+
+    it("merges overlapping ranges", () => {
+      const set = new RangeSet();
+
+      set.addRange([1, 3]);
+      expect(set.ranges).toEqual([[1, 3]]);
+
+      set.addRange([2, 4]);
+      expect(set.ranges).toEqual([[1, 4]]);
+
+      set.addRange([6, 8]);
+      expect(set.ranges).toEqual([
+        [1, 4],
+        [6, 8],
+      ]);
+
+      set.addRange([3, 7]);
+      expect(set.ranges).toEqual([[1, 8]]);
+    });
+
+    it("merges adjacent ranges", () => {
+      const set = new RangeSet();
+
+      set.addRange([1, 2]);
+      expect(set.ranges).toEqual([[1, 2]]);
+
+      set.addRange([2, 3]);
+      expect(set.ranges).toEqual([[1, 3]]);
+
+      set.addRange([5, 6]);
+      expect(set.ranges).toEqual([
+        [1, 3],
+        [5, 6],
+      ]);
+
+      set.addRange([3, 5]);
+      expect(set.ranges).toEqual([[1, 6]]);
+    });
+
+    it("throws an error if an invalid range is added", () => {
+      const set = new RangeSet();
+      expect(() => set.addRange([3, 2])).toThrow("Invalid Range");
+    });
+  });
+
+  describe("hasRange", () => {
+    it("returns true for inputs matching ranges in the set", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]);
+      expect(set.hasRange([1, 2])).toBe(true);
+      expect(set.hasRange([3, 4])).toBe(true);
+      expect(set.hasRange([5, 6])).toBe(true);
+    });
+
+    it("returns true for inputs that are sub-ranges of ranges in the set", () => {
+      const set = new RangeSet([[3, 7]]);
+      expect(set.hasRange([3, 5])).toBe(true);
+      expect(set.hasRange([4, 6])).toBe(true);
+      expect(set.hasRange([5, 7])).toBe(true);
+    });
+
+    it("returns false for inputs that are not in the set", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [5, 7],
+      ]);
+      expect(set.hasRange([0, 1])).toBe(false);
+      expect(set.hasRange([2, 5])).toBe(false);
+      expect(set.hasRange([7, 9])).toBe(false);
+      expect(set.hasRange([3, 4])).toBe(false);
+      expect(set.hasRange([100, 200])).toBe(false);
+    });
+
+    it("returns false for inputs that extend beyond ranges in the set", () => {
+      const set = new RangeSet([[4, 6]]);
+      expect(set.hasRange([3, 5])).toBe(false);
+      expect(set.hasRange([3, 7])).toBe(false);
+      expect(set.hasRange([5, 7])).toBe(false);
+    });
+
+    it("returns false for inputs that have gaps in the set", () => {
+      const set = new RangeSet([
+        [1, 3],
+        [6, 9],
+      ]);
+      expect(set.hasRange([1, 7])).toBe(false);
+      expect(set.hasRange([1, 9])).toBe(false);
+      expect(set.hasRange([2, 9])).toBe(false);
+    });
+
+    it("throws an error if an invalid range is checked", () => {
+      const set = new RangeSet();
+      expect(() => set.hasRange([-1, -2])).toThrow("Invalid Range");
+    });
+  });
+});
