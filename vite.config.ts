1b6bbfa2a1f0e36b0995da198d24f8d1acb09943
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-11 00:10:22 -0500

    refactor: 🏗️ use vite for building app

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config";
+import dts from "unplugin-dts/vite";
 
 export default defineConfig({
+  plugins: [dts({ exclude: "**/*.test.ts" })],
+  build: {
+    lib: {
+      entry: {
+        index: "src/index.ts",
+      },
+      formats: ["es"],
+    },
+  },
   test: {
     coverage: {
       provider: "v8",
