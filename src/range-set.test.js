adc913fca1d9580b396f4574be003ae53709fcb0
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-01-11 12:23:44 -0500

    refactor: ♻️ change method order in RangeSet

--- src/range-set.test.js
+++ src/range-set.test.js
@@ -106,6 +106,60 @@ describe("RangeSet", () => {
     });
   });
 
+  describe("hasRange", () => {
+    it("returns true for inputs matching ranges in the set", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]);
+      expect(set.hasRange([1, 2])).toBe(true);
+      expect(set.hasRange([3, 4])).toBe(true);
+      expect(set.hasRange([5, 6])).toBe(true);
+    });
+
+    it("returns true for inputs that are sub-ranges of ranges in the set", () => {
+      const set = new RangeSet([[3, 7]]);
+      expect(set.hasRange([3, 5])).toBe(true);
+      expect(set.hasRange([4, 6])).toBe(true);
+      expect(set.hasRange([5, 7])).toBe(true);
+    });
+
+    it("returns false for inputs that are not in the set", () => {
+      const set = new RangeSet([
+        [1, 2],
+        [5, 7],
+      ]);
+      expect(set.hasRange([0, 1])).toBe(false);
+      expect(set.hasRange([2, 5])).toBe(false);
+      expect(set.hasRange([7, 9])).toBe(false);
+      expect(set.hasRange([3, 4])).toBe(false);
+      expect(set.hasRange([100, 200])).toBe(false);
+    });
+
+    it("returns false for inputs that extend beyond ranges in the set", () => {
+      const set = new RangeSet([[4, 6]]);
+      expect(set.hasRange([3, 5])).toBe(false);
+      expect(set.hasRange([3, 7])).toBe(false);
+      expect(set.hasRange([5, 7])).toBe(false);
+    });
+
+    it("returns false for inputs that have gaps in the set", () => {
+      const set = new RangeSet([
+        [1, 3],
+        [6, 9],
+      ]);
+      expect(set.hasRange([1, 7])).toBe(false);
+      expect(set.hasRange([1, 9])).toBe(false);
+      expect(set.hasRange([2, 9])).toBe(false);
+    });
+
+    it("throws an error if an invalid range is checked", () => {
+      const set = new RangeSet();
+      expect(() => set.hasRange([-1, -2])).toThrow("Invalid Range");
+    });
+  });
+
   describe("addRange", () => {
     it("adds a range to the set", () => {
       const set = new RangeSet();
@@ -186,60 +240,6 @@ describe("RangeSet", () => {
     it("throws an error if an invalid range is added", () => {
       const set = new RangeSet();
       expect(() => set.addRange([3, 2])).toThrow("Invalid Range");
-    });
-  });
-
-  describe("hasRange", () => {
-    it("returns true for inputs matching ranges in the set", () => {
-      const set = new RangeSet([
-        [1, 2],
-        [3, 4],
-        [5, 6],
-      ]);
-      expect(set.hasRange([1, 2])).toBe(true);
-      expect(set.hasRange([3, 4])).toBe(true);
-      expect(set.hasRange([5, 6])).toBe(true);
-    });
-
-    it("returns true for inputs that are sub-ranges of ranges in the set", () => {
-      const set = new RangeSet([[3, 7]]);
-      expect(set.hasRange([3, 5])).toBe(true);
-      expect(set.hasRange([4, 6])).toBe(true);
-      expect(set.hasRange([5, 7])).toBe(true);
-    });
-
-    it("returns false for inputs that are not in the set", () => {
-      const set = new RangeSet([
-        [1, 2],
-        [5, 7],
-      ]);
-      expect(set.hasRange([0, 1])).toBe(false);
-      expect(set.hasRange([2, 5])).toBe(false);
-      expect(set.hasRange([7, 9])).toBe(false);
-      expect(set.hasRange([3, 4])).toBe(false);
-      expect(set.hasRange([100, 200])).toBe(false);
-    });
-
-    it("returns false for inputs that extend beyond ranges in the set", () => {
-      const set = new RangeSet([[4, 6]]);
-      expect(set.hasRange([3, 5])).toBe(false);
-      expect(set.hasRange([3, 7])).toBe(false);
-      expect(set.hasRange([5, 7])).toBe(false);
-    });
-
-    it("returns false for inputs that have gaps in the set", () => {
-      const set = new RangeSet([
-        [1, 3],
-        [6, 9],
-      ]);
-      expect(set.hasRange([1, 7])).toBe(false);
-      expect(set.hasRange([1, 9])).toBe(false);
-      expect(set.hasRange([2, 9])).toBe(false);
-    });
-
-    it("throws an error if an invalid range is checked", () => {
-      const set = new RangeSet();
-      expect(() => set.hasRange([-1, -2])).toThrow("Invalid Range");
     });
   });
 });
